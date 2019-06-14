291cd5755fb16776406473b235370ad7659b18c4
imran <imranchowdhury14@gmail.com>
2019-06-13 20:23:32 -0400

    Image Update

--- app/src/main/java/com/example/safeparkingapp/menu.java
+++ app/src/main/java/com/example/safeparkingapp/menu.java
@@ -4,14 +4,34 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class menu extends AppCompatActivity {
+    private static final String TAG = "menu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Log.d(TAG, "onCreate: Started");
+
+        //First Image
+        ImageView firstImage = (ImageView) findViewById(R.id.firstImage);
+        int imageResources = getResources().getIdentifier("@drawable/parking", null, this.getPackageName());
+        firstImage.setImageResource(imageResources);
+
+
+       //Second Image
+        ImageView image2 = (ImageView) findViewById(R.id.imageView2);
+        int imageResources2 = getResources().getIdentifier("@drawable/app", null, this.getPackageName());
+        firstImage.setImageResource(imageResources);
+
+
+
+
+
     }
 
     public void sendToMap(View view) {
